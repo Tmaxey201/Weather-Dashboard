3be167076f1a6774f35898e6f5c46ac32cf2ea39
Thomas Maxey <tmaxey201@gmail.com>
2021-05-05 15:45:09 -0400

    added HTML to create search bar and header

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+var apiKey = 'beb391db5643b48362758c5f3a3a25ce'
+var currentWeather = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}'
+var fiveDayforecast = 'api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}'
